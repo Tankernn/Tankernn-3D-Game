8f1a63e0c26cf80bf4a16b2ee9beaba7ec2142fb
Tankernn <frans@tankernn.eu>
2017-03-22 09:46:19 +0100

    Skybox texture oreder fix

--- src/main/java/eu/tankernn/game/Settings.java
+++ src/main/java/eu/tankernn/game/Settings.java
@@ -3,11 +3,12 @@ package eu.tankernn.game;
 public class Settings {
 	public static final boolean DEBUG = false;
 	public static final String GAME_NAME = "Tankernn game engine tester";
-	
+
 	// Skybox settings
-	public static final String[] TEXTURE_FILES = {"alps_rt", "alps_lf", "alps_up", "alps_dn", "alps_bk", "alps_ft"};
-	public static final String[] NIGHT_TEXTURE_FILES = {"midnight_rt", "midnight_lf", "midnight_up", "midnight_dn", "midnight_bk", "midnight_ft"};
-	
+	public static final String[] TEXTURE_FILES = { "alps_ft", "alps_bk", "alps_up", "alps_dn", "alps_rt", "alps_lf" };
+	public static final String[] NIGHT_TEXTURE_FILES = { "midnight_ft", "midnight_bk", "midnight_up", "midnight_dn",
+			"midnight_rt", "midnight_lf" };
+
 	// Water settings
 	public static final String DUDV_MAP = "textures/waterDUDV.png";
 	public static final String NORMAL_MAP = "textures/waterNormalMap.png";
